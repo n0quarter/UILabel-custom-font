b8b1032130becb5257b3ce49ed647cde282064b1
Viktor Shcherban <a@asdCode.com>
2013-09-03 11:57:52 +0300

    minor fixes

--- UILabel+font.m
+++ UILabel+font.m
@@ -8,15 +8,19 @@
 
 #import "UILabel+font.h"
 
+#define BoldFontName  @"NeuronHeavy"
+#define PlainFontName @"Neuron"
+
+
 @implementation UILabel (myCustomFont)
 
 - (void)awakeFromNib {
     [super awakeFromNib];
     
     if ([self.font.fontName rangeOfString:@"bold"].length > 0 || [self.font.fontName rangeOfString:@"Bold"].length > 0)
-        [self setFont:[UIFont fontWithName:@"NeuronHeavy" size:self.font.pointSize]];
+        [self setFont:[UIFont fontWithName:BoldFontName size:self.font.pointSize]];
     else
-        [self setFont:[UIFont fontWithName:@"Neuron" size:self.font.pointSize]];
+        [self setFont:[UIFont fontWithName:PlainFontName size:self.font.pointSize]];
     
 }
 
@@ -24,9 +28,9 @@
     id result = [super initWithFrame:frame];
     if (result) {
         if ([self.font.fontName rangeOfString:@"bold"].length > 0 || [self.font.fontName rangeOfString:@"Bold"].length > 0)
-            [self setFont:[UIFont fontWithName:@"NeuronHeavy" size:self.font.pointSize]];
+            [self setFont:[UIFont fontWithName:BoldFontName size:self.font.pointSize]];
         else
-            [self setFont:[UIFont fontWithName:@"Neuron" size:self.font.pointSize]];
+            [self setFont:[UIFont fontWithName:PlainFontName size:self.font.pointSize]];
     }
     return result;
 }
